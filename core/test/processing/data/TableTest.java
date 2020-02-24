2c9b537fcc8242868ff7a7ee426eceffa4fb9ea0
Liam James <liam@minimaximize.com>
2020-02-25 07:26:53 +1000

    remove comments

---
+++ core/test/processing/data/TableTest.java
@@ -0,0 +1,36 @@
+package processing.data;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TableTest {
+
+    class Person {
+        public String name;
+        public int age;
+
+        public Person() {
+            name = "";
+            age = -1;
+        }
+    }
+
+
+    Person[] people;
+
+    @Test
+    public void parseInto() {
+        Table table = new Table();
+        table.addColumn("name");
+        table.addColumn("age");
+
+        TableRow row = table.addRow();
+        row.setString("name", "Person1");
+        row.setInt("age", 30);
+
+        table.parseInto(this, "people");
+
+        Assert.assertEquals(people[0].name, "Person1");
+        Assert.assertEquals(people[0].age, 30);
+    }
+}
